c568bb1e380a441d777d453e6e5d580b8b11d4e2
Samii Shabuse <sshabuse@gmail.com>
2025-08-05 00:06:25 -0400

    refactor: created separate functions for calculate & readNumber

--- src/Main.hs
+++ src/Main.hs
@@ -1,5 +1,23 @@
 module Main (main) where
 
+-- Function to perform the calculator operations
+calculate :: Double -> String -> Double -> String
+calculate a operation b =
+  case operation of
+    "+" -> show (a + b)
+    "-" -> show (a - b)
+    "*" -> show (a * b)
+    "/" -> if b /= 0 then show (a / b) else "Error: Division by zero"
+    _   -> "Error: Invalid operation"
+
+-- Helper function to read a number from the user
+readNumber :: String -> IO Double
+readNumber prompt = do
+  putStrLn prompt
+  input <- getLine
+  return (read input :: Double)
+
+-- Main entry point
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell World!"
@@ -7,27 +25,15 @@ main = do
   name <- getLine
   putStrLn ("Nice to meet you, " ++ name ++ "! Welcome to Haskell calculator! <Insert Calculator Emoji!>")
 
-  putStrLn "Enter the first number: "
-  firstInput <- getLine
+  firstInput <- readNumber "Please enter the first number: "
 
-  putStrLn "Enter the second number: "
-  secondInput <- getLine
+  secondInput <- readNumber "Please enter the second number: "
 
   putStrLn "Choose an operation (+, -, *, /): "
   operation <- getLine
 
-  let firstNumber = read firstInput :: Int
-      secondNumber = read secondInput :: Int
-      
-      result = case operation of
-        "+" -> firstNumber + secondNumber
-        "-" -> firstNumber - secondNumber
-        "*" -> firstNumber * secondNumber
-        "/" -> if secondNumber /= 0 
-                  then firstNumber `div` secondNumber 
-                  else error "Cannot divide by zero!"
-        _   -> error "Invalid operation!"
+  let result = calculate firstInput operation secondInput
 
   -- This is a comment explaining the next line of code
-  putStrLn ("The result of " ++ show firstNumber ++ " " ++ operation ++ " " ++ show secondNumber ++ " is " ++ show result)
+  putStrLn ("The result of " ++ show firstInput ++ " " ++ operation ++ " " ++ show secondInput ++ " is " ++ show result)
   putStrLn "Thank you for using the Haskell calculator. Goodbye!"
