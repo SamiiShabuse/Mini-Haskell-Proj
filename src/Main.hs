2cd1215b5802737c97db10717c46d691cb36e950
Samii Shabuse <sshabuse@gmail.com>
2025-08-04 20:23:16 -0400

    feat: initial introduction of calculator

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,8 @@
 module Main (main) where
-import GHC.IO.Handle (nativeNewline)
 
 main :: IO ()
 main = do
   putStrLn "Hello, Haskell World!"
   putStrLn "What is your name?"
   name <- getLine
-  putStrLn ("Nice to meet you, " ++ name ++ "!")
-  putStrLn ("Your system's native newline character is: " ++ show nativeNewline)
+  putStrLn ("Nice to meet you, " ++ name ++ "! Welcome to Haskell calculator! <Insert Calculator Emoji!>")
