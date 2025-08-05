69ec40b2e3005edde7d20ab98a5d471867a2ed01
Samii Shabuse <sshabuse@gmail.com>
2025-08-04 20:50:18 -0400

    feat: made base calculator to take in int input

--- src/Main.hs
+++ src/Main.hs
@@ -6,3 +6,17 @@ main = do
   putStrLn "What is your name?"
   name <- getLine
   putStrLn ("Nice to meet you, " ++ name ++ "! Welcome to Haskell calculator! <Insert Calculator Emoji!>")
+
+  putStrLn "Enter the first number: "
+  firstInput <- getLine
+
+  putStrLn "Enter the second number: "
+  secondInput <- getLine
+
+  let firstNumber = read firstInput :: Int
+      secondNumber = read secondInput :: Int
+      sumResult = firstNumber + secondNumber
+
+  -- This is a comment explaining the next line of code
+  putStrLn ("The sum of " ++ show firstNumber ++ " and " ++ show secondNumber ++ " is " ++ show sumResult)
+  putStrLn "Thank you for using the Haskell calculator. Goodbye!"
